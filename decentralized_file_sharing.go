ec5d8fae17aeca019f5e1a21eabae96a7e871818
antonvasia <antonvasia130@gmail.com>
2023-05-11 14:00:01 +0200

    Update testing framework for Solidity

--- decentralized_file_sharing.go
+++ decentralized_file_sharing.go
@@ -1,3 +1,4 @@
 // Add support for DeFi lending protocols
 // Implement decentralized arbitration in contracts
 // Add support for decentralized marketplaces
+// Fix issues with contract finalization
