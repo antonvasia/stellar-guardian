7a50e194e61f1a98191d369488dae1e4c185f859
antonvasia <antonvasia130@gmail.com>
2023-06-23 14:00:01 +0200

    Enhance contract upgrade process

--- cross_chain_bridge.sol
+++ cross_chain_bridge.sol
@@ -1,3 +1,4 @@
 // Integrate with decentralized identity providers
 // Add governance proposal expiration feature
 // Add support for delegation in governance
+// Refactor contract to use EIP-712 signatures
