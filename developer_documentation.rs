d3bc4a3f4020e0054fb2f924f86a176edbc85cf8
antonvasia <antonvasia130@gmail.com>
2023-08-13 14:00:02 +0200

    Reduce gas cost in minting logic

--- developer_documentation.rs
+++ developer_documentation.rs
@@ -1,3 +1,4 @@
 // Enhance decentralized autonomous governance
 // Improve decentralized oracle protocols
 // Enhance decentralized finance (DeFi) ecosystem
+// Update testing framework for Solidity
