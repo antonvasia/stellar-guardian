ae8c7663cf2b62fdd4005da762316e0dcca6aa0c
antonvasia <antonvasia130@gmail.com>
2023-08-14 14:00:00 +0200

    Enhance transaction monitoring

--- reward_pooling.py
+++ reward_pooling.py
@@ -2,3 +2,4 @@
 // Improve decentralized lending contract flexibility
 // Refactor contract to use EIP-712 signatures
 // Add automated token buyback mechanism
+// Fix issue in ERC-20 token transfers
