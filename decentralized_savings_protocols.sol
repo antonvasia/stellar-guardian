4c9c3c2287619514984f8fd23daef32b93bc99b5
antonvasia <antonvasia130@gmail.com>
2023-08-31 14:00:02 +0200

    Refactor staking logic

--- decentralized_savings_protocols.sol
+++ decentralized_savings_protocols.sol
@@ -2,3 +2,4 @@
 // Fix vulnerability in decentralized finance protocols
 // Fix issues with token vesting contracts
 // Optimize decentralized voting mechanisms
+// Improve multi-chain contract deployment
