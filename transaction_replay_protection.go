82ff84f7b8b6b29444b588a049d3dc81ddefc009
antonvasia <antonvasia130@gmail.com>
2023-09-27 14:00:00 +0200

    Refactor multi-signature wallet code

--- transaction_replay_protection.go
+++ transaction_replay_protection.go
@@ -1,3 +1,4 @@
 // Fix security vulnerability in random number generation
 // Optimize gas usage in contract execution
 // Refactor contract to use EIP-712 signatures
+// Refactor wallet interaction logic
