57cc95f33f8c49cbec217bff1475f899c28f92fd
antonvasia <antonvasia130@gmail.com>
2023-10-04 14:00:00 +0200

    Improve staking pool reward management

--- reward_escrow.sol
+++ reward_escrow.sol
@@ -1,3 +1,4 @@
 // Add security checks for contract deployment
 // Add security checks for contract deployment
 // Refactor contract to use EIP-712 signatures
+// Implement cross-platform contract compatibility
