e7db568240229a0b74a05e9b245563f051d01568
antonvasia <antonvasia130@gmail.com>
2023-10-14 14:00:00 +0200

    Enhance governance contract flexibility

--- blockchain_ledger_security.go
+++ blockchain_ledger_security.go
@@ -1,3 +1,4 @@
 // Implement liquidity pool staking mechanism
 // Add support for decentralized file storage
 // Fix scalability issues with smart contracts
+// Implement a new contract event system
