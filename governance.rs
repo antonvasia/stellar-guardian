1ec422f90c4b3cc1b56043d6e1369e02190e4d8e
antonvasia <antonvasia130@gmail.com>
2023-10-25 14:00:00 +0200

    Fix governance proposal approval logic

--- governance.rs
+++ governance.rs
@@ -1,3 +1,4 @@
 // Improve security for token minting contracts
 // Fix compatibility issues with MetaMask
 // Fix issue with multi-chain transactions
+// Fix interoperability issues with other blockchains
