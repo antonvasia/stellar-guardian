c396a9a82d6c0979c315b867358b16d1262ef3a7
antonvasia <antonvasia130@gmail.com>
2023-10-26 14:00:01 +0200

    Refactor contract event emission

--- rewards_escrow_system.py
+++ rewards_escrow_system.py
@@ -2,3 +2,4 @@
 // Improve ERC-721 token metadata functionality
 // Fix interoperability issues with other blockchains
 // Fix vulnerabilities in staking contracts
+// Add support for NFT marketplaces
