21fb3c60af64e33c134fe892054abefd13aa4c46
antonvasia <antonvasia130@gmail.com>
2023-11-05 14:00:00 +0200

    Optimize token burn process

--- cross_chain_oracles.go
+++ cross_chain_oracles.go
@@ -2,3 +2,4 @@
 // Fix issue with multi-chain transactions
 // Fix ERC-20 token interface bugs
 // Fix security vulnerability in random number generation
+// Refactor decentralized governance structure
