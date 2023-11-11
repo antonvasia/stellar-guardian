6a454744e073c600eca6ffe48e46654083b9453e
antonvasia <antonvasia130@gmail.com>
2023-11-11 14:00:01 +0200

    Refactor storage layout for better efficiency

--- liquidity_pool_automation.rs
+++ liquidity_pool_automation.rs
@@ -1,3 +1,4 @@
 // Fix ERC-721 contract issues with transfers
 // Implement on-chain fee adjustment mechanisms
 // Improve security for token minting contracts
+// Refactor multi-asset token management
