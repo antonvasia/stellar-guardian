d5d4022a2b4817052130c79c63264c69e96d7bc6
antonvasia <antonvasia130@gmail.com>
2023-11-25 14:00:02 +0200

    Improve ERC-721 token metadata functionality

--- nft_protocol_integration.py
+++ nft_protocol_integration.py
@@ -1,3 +1,4 @@
 // Improve gas estimation for transactions
 // Integrate with decentralized identity providers
 // Fix issue in ERC-20 token transfers
+// Integrate with decentralized prediction markets
