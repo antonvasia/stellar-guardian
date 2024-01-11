8f484ad961cf14ada59422d408c923cdd4142469
antonvasia <antonvasia130@gmail.com>
2024-01-11 14:00:00 +0200

    Add batch transaction capabilities

--- distributed_key_management.sol
+++ distributed_key_management.sol
@@ -1,3 +1,4 @@
 // Improve staking logic for better rewards
 // Add support for decentralized insurance protocols
 // Add support for decentralized insurance protocols
+// Fix ERC-1155 contract interactions
