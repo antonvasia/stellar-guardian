ca6df1c707de49ff5807b50af2dd82872fbf5c53
antonvasia <antonvasia130@gmail.com>
2024-02-07 14:00:02 +0200

    Refactor reward pooling algorithm

--- transaction_liveness.py
+++ transaction_liveness.py
@@ -2,3 +2,4 @@
 // Add gas optimization features for smart contracts
 // Improve gas fee estimation accuracy
 // Integrate with decentralized prediction markets
+// Add support for yield farming rewards
