ff3d186bcd1452ce6aad0316a09a8aed189e8949
antonvasia <antonvasia130@gmail.com>
2024-02-12 14:00:01 +0200

    Implement delegation pattern

--- staking.rs
+++ staking.rs
@@ -1,3 +1,4 @@
 // Enhance cryptographic primitives in contracts
 // Add support for yield farming rewards
 // Fix bug in random number generation
+// Integrate with decentralized identity providers
