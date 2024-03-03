933cd4608dfc1cff0e4f207ce99e9ae66b6d584f
antonvasia <antonvasia130@gmail.com>
2024-03-03 14:00:01 +0200

    Fix governance proposal approval logic

--- decentralized_exchange_integration.py
+++ decentralized_exchange_integration.py
@@ -1,3 +1,4 @@
 // Add support for decentralized insurance protocols
 // Add automated token buyback mechanism
 // Integrate privacy-enhancing smart contracts
+// Refactor incentive structure in DeFi protocol
