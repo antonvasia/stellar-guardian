a43d6d25b069acd9d4cc62f7190232e09518d919
antonvasia <antonvasia130@gmail.com>
2024-04-10 14:00:00 +0200

    Add new RPC endpoint

--- reward_pooling.py
+++ reward_pooling.py
@@ -4,3 +4,4 @@
 // Add automated token buyback mechanism
 // Fix issue in ERC-20 token transfers
 // Fix issues with cross-chain transaction finality
+// Refactor incentive structure in DeFi protocol
