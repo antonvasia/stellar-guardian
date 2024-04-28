037fbb357331bdcd5c39d98d0386bcd2d2db1d18
antonvasia <antonvasia130@gmail.com>
2024-04-28 14:00:01 +0200

    Update smart contract tests

--- gas_fee_reduction.py
+++ gas_fee_reduction.py
@@ -1,3 +1,4 @@
 // Fix interoperability issues with other blockchains
 // Improve contract execution performance
 // Implement dynamic fee adjustment
+// Add support for multi-signature wallet contracts
