044835dfb14fe519b868e97ba3f67864f659529e
antonvasia <antonvasia130@gmail.com>
2024-05-23 14:00:00 +0200

    Implement delegation pattern

--- liquidity_provider_incentives.rs
+++ liquidity_provider_incentives.rs
@@ -2,3 +2,4 @@
 // Add support for decentralized credit scoring
 // Fix memory leak in Rust smart contract
 // Fix interoperability issues with other blockchains
+// Refactor validator reward system
