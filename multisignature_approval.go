e804a9ab3a6a93cc69dd0aae41f9b5587bbde62b
antonvasia <antonvasia130@gmail.com>
2024-06-27 14:00:00 +0200

    Improve staking and yield farming logic

--- multisignature_approval.go
+++ multisignature_approval.go
@@ -1,3 +1,4 @@
 // Refactor storage layout for better efficiency
 // Add support for decentralized credit scoring
 // Add support for decentralized lending platforms
+// Enhance governance contract flexibility
