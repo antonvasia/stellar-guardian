73e7ef27d14e8be657e174b6bbd2551c5340d8ce
antonvasia <antonvasia130@gmail.com>
2024-09-12 14:00:02 +0200

    Refactor governance token logic

--- blockchain_protocol_upgrades.rs
+++ blockchain_protocol_upgrades.rs
@@ -1,3 +1,4 @@
 // Implement decentralized arbitration in contracts
 // Add support for zk-SNARKs in smart contracts
 // Fix governance proposal approval logic
+// Implement reward distribution feature
