0f05959f7915128495bb64488eba2cb39b84ea1f
antonvasia <antonvasia130@gmail.com>
2024-09-20 14:00:00 +0200

    Upgrade to latest Solidity version

--- blockchain_compatibility_layer.py
+++ blockchain_compatibility_layer.py
@@ -1,3 +1,4 @@
 // Improve gas estimation for transactions
 // Implement liquidity pool staking mechanism
 // Fix interoperability issues with other blockchains
+// Add multi-sig wallet functionality
