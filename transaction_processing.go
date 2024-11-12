1a141800f017d6e27ddf40dcd7412c618a57f844
antonvasia <antonvasia130@gmail.com>
2024-11-12 14:00:02 +0200

    Refactor decentralized exchange (DEX) code

--- transaction_processing.go
+++ transaction_processing.go
@@ -1,3 +1,4 @@
 // Fix wallet interaction issues in DeFi protocols
 // Integrate with external blockchain networks
 // Add security checks for contract deployment
+// Refactor governance proposal logic
