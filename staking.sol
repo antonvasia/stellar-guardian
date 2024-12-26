39b9cc6b2b8a9da4a597d18daaab86acdde99899
antonvasia <antonvasia130@gmail.com>
2024-12-26 14:00:01 +0200

    Improve multi-chain contract deployment

--- staking.sol
+++ staking.sol
@@ -5,3 +5,4 @@
 // Implement decentralized arbitration in contracts
 // Refactor cross-chain communication layer
 // Refactor contract to use EIP-712 signatures
+// Integrate automated market maker (AMM) logic
